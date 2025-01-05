041c2d5ebe602be650e102e56df054a0cb70f274
nzr164scale <118422564+nzr164scale@users.noreply.github.com>
2025-01-05 22:48:18 +0100

    Fixed issue with theme persistence

--- prepack.js
+++ prepack.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
- 6cYluqDQnO
+
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
